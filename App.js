a654f13afbdd0925995785836778da6e46a6141a
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-12-27 21:46:41 +0500

    button add

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 30 }}>
+       <View> 
+         <TextInput placeholder="Search Here" style={{borderColor:"Blue", borderWidth:1 , padding: 10}}/>
+            <button title="ADD"/>
+           </View> 
+           <View>
+
+           </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
+  
 });
